b8e6272d9a42df2101e84b93047dcddb777b92c4
Joe Elliott <number101010@gmail.com>
2019-09-04 15:48:58 -0400

    First pass fleshed out HierarchicalFSObjectClient
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- pkg/chunk/local/hierarchical_fs_object_client.go
+++ pkg/chunk/local/hierarchical_fs_object_client.go
@@ -6,11 +6,17 @@ import (
 
 	"github.com/cortexproject/cortex/pkg/chunk"
 	"github.com/cortexproject/cortex/pkg/chunk/util"
+	"hash/fnv"
+)
+
+const (
+	folderCount = uint64(10000)
 )
 
 // FSObjectClient holds config for filesystem as object store
 type HierarchicalFSObjectClient struct {
-	cfg FSConfig
+	cfg FSConfig,
+	hash hash.Hash64,
 }
 
 // NewFSObjectClient makes a chunk.ObjectClient which stores chunks as files in the local filesystem.
@@ -21,6 +27,7 @@ func NewHierarchicalFSObjectClient(cfg FSConfig) (*HierarchicalFSObjectClient, e
 
 	return &HierarchicalFSObjectClient{
 		cfg: cfg,
+		hash: fnv.New64a(),
 	}, nil
 }
 
@@ -28,7 +35,17 @@ func NewHierarchicalFSObjectClient(cfg FSConfig) (*HierarchicalFSObjectClient, e
 func (HierarchicalFSObjectClient) Stop() {}
 
 // PutChunks implements ObjectClient
-func (f *HierarchicalFSObjectClient) PutChunks(_ context.Context, chunks []chunk.Chunk) error {
+func (f *HierarchicalFSObjectClient) PutChunks(_ context.Context, chunks []chunk.Chunk) error {	for i := range chunks {
+		buf, err := chunks[i].Encoded()
+		if err != nil {
+			return err
+		}
+
+		filename := f.filenameFromChunk(chunks[i])
+		if err := ioutil.WriteFile(filename, buf, 0644); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -38,10 +55,35 @@ func (f *HierarchicalFSObjectClient) GetChunks(ctx context.Context, chunks []chu
 }
 
 func (f *HierarchicalFSObjectClient) getChunk(_ context.Context, decodeContext *chunk.DecodeContext, c chunk.Chunk) (chunk.Chunk, error) {
-	return chunk.Chunk{}, nil
+	filename := f.filenameFromChunk(c)
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return c, err
+	}
+
+	if err := c.Decode(decodeContext, buf); err != nil {
+		return c, err
+	}
+
+	return c, nil
 }
 
 // DeleteChunksBefore implements BucketClient
 func (f *HierarchicalFSObjectClient) DeleteChunksBefore(ctx context.Context, ts time.Time) error {
-	return nil
+	return filepath.Walk(f.cfg.Directory, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && info.ModTime().Before(ts) {
+			level.Info(pkgUtil.Logger).Log("msg", "file has exceeded the retention period, removing it", "filepath", info.Name())
+			if err := os.Remove(path); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+}
+
+func (f *HierarchicalFSObjectClient) filenameFromChunk(c chunk.Chunk) string {
+	folderName := fmt.Sprintf("%x", f.hash(c.ExternalKey) % folderCount)
+
+	return path.Join(f.cfg.Directory, folderName, base64.StdEncoding.EncodeToString([]byte(chunks[i].ExternalKey())) 
 }
